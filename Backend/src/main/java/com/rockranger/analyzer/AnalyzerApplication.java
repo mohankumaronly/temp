0fe2d5a2486d2f7ccfa2057b5dc78040f7317c5f
mohankumaronly <mohankumaronly81@gmail.com>
2026-08-23 19:51:51 +0530

    Register fuction is implimented

--- Backend/src/main/java/com/rockranger/analyzer/AnalyzerApplication.java
+++ Backend/src/main/java/com/rockranger/analyzer/AnalyzerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AnalyzerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AnalyzerApplication.class, args);
 	}
-
 }
--- Backend/src/main/java/com/rockranger/analyzer/authentication/service/impl/AuthenticationServiceImpl.java
+++ Backend/src/main/java/com/rockranger/analyzer/authentication/service/impl/AuthenticationServiceImpl.java
@@ -6,15 +6,47 @@ import com.rockranger.analyzer.authentication.dto.request.VerifyOtpRequest;
 import com.rockranger.analyzer.authentication.dto.response.LoginResponse;
 import com.rockranger.analyzer.authentication.dto.response.RegisterResponse;
 import com.rockranger.analyzer.authentication.dto.response.VerifyOtpResponse;
+import com.rockranger.analyzer.authentication.entity.User;
+import com.rockranger.analyzer.authentication.repository.UserRepository;
 import com.rockranger.analyzer.authentication.service.AuthenticationService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
 
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public AuthenticationServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     public RegisterResponse register(RegisterRequest registerRequest) {
-        return null;
+
+        if (userRepository.existsByEmail(registerRequest.getEmail())) {
+            RegisterResponse response = new RegisterResponse();
+            response.setMessage("Email is already registered.");
+            return response;
+        }
+
+        User user = new User();
+
+        user.setFullName(registerRequest.getFullName());
+        user.setEmail(registerRequest.getEmail());
+
+        user.setPasswordHash(
+                passwordEncoder.encode(registerRequest.getPassword())
+        );
+
+        userRepository.save(user);
+
+        RegisterResponse response = new RegisterResponse();
+        response.setMessage("Account created successfully.");
+
+        return response;
     }
 
     @Override
