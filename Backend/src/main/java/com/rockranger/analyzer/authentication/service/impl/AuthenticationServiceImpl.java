41fc45a9a83c8feda478a405f6597101fb7d4f19
mohankumaronly <mohankumaronly81@gmail.com>
2026-08-25 15:22:36 +0530

    Updating: Register function is updating

--- Backend/src/main/java/com/rockranger/analyzer/authentication/service/impl/AuthenticationServiceImpl.java
+++ Backend/src/main/java/com/rockranger/analyzer/authentication/service/impl/AuthenticationServiceImpl.java
@@ -6,21 +6,28 @@ import com.rockranger.analyzer.authentication.dto.request.VerifyOtpRequest;
 import com.rockranger.analyzer.authentication.dto.response.LoginResponse;
 import com.rockranger.analyzer.authentication.dto.response.RegisterResponse;
 import com.rockranger.analyzer.authentication.dto.response.VerifyOtpResponse;
+import com.rockranger.analyzer.authentication.entity.EmailVerificationOtp;
 import com.rockranger.analyzer.authentication.entity.User;
+import com.rockranger.analyzer.authentication.repository.EmailVerificationOtpRepository;
 import com.rockranger.analyzer.authentication.repository.UserRepository;
 import com.rockranger.analyzer.authentication.service.AuthenticationService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService {
 
     private UserRepository userRepository;
     private PasswordEncoder passwordEncoder;
+    private EmailVerificationOtpRepository emailVerificationOtpRepository;
 
-    public AuthenticationServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public AuthenticationServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder, EmailVerificationOtpRepository emailVerificationOtpRepository) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.emailVerificationOtpRepository = emailVerificationOtpRepository;
     }
 
     @Override
@@ -43,8 +50,22 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
         userRepository.save(user);
 
+        String otp = String.valueOf(
+                ThreadLocalRandom.current().nextInt(100000, 1000000)
+        );
+
+        System.out.println("OTP for " + user.getEmail() + ": " + otp);
+
+        EmailVerificationOtp otpEntity = new EmailVerificationOtp();
+
+        otpEntity.setUser(user);
+        otpEntity.setOtpHash(passwordEncoder.encode(otp));
+        otpEntity.setExpiresAt(LocalDateTime.now().plusMinutes(5));
+
+        emailVerificationOtpRepository.save(otpEntity);
+
         RegisterResponse response = new RegisterResponse();
-        response.setMessage("Account created successfully.");
+        response.setMessage("OTP has been sent to your email.");
 
         return response;
     }
